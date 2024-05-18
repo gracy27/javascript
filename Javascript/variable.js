b7153bae21e3f94ef93dbe81c2069cb69489380d
gracy27 <122380217+gracy27@users.noreply.github.com>
2024-05-18 12:06:30 +0000

    variable

---
+++ Javascript/variable.js
@@ -0,0 +1,16 @@
+const accountId=144552
+let accountEmail="gracy2bc123@gmail.com"
+var accountPassword="1234"
+accountCity="Delhi"
+let accountState;
+
+
+//accountId=29876          // not allowed 
+accountEmail="abc@example.com"
+accountPassword="89"
+accountCity="Mumbai"
+//console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+
