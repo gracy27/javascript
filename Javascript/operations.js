177fa49965fd129bc819bb3a572c03c0b3ab0426
gracy27 <122380217+gracy27@users.noreply.github.com>
2024-05-18 18:26:22 +0000

    Operators

---
+++ Javascript/operations.js
@@ -0,0 +1,17 @@
+// CONCATENATION
+let str1="gracy"
+let str2="hello"
+let str3=str2+" "+str1
+
+// HOW THIS IS HAPPENING
+console.log(str3)
+console.log("1"+2)
+console.log("2"+1)
+console.log("2"+1+1)
+console.log(2+2+"1")
+
+// TRICKY CONVERSION  // NOT GOOD WAY OF WRITING CODE IT SHOULD BE READABLE AND CLEAN
+let num1,num2,num3
+num1=num2=num3=2+2
+
+// PREFIX ANFD POSTFIX OPERATOR READ YOURSELF
